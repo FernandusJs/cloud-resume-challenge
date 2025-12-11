b18b3d0528c8d2cbd2fe0daea276dbd0ac231199
Nand Beeckx <nand.beeckx@gmail.com>
2025-12-11 14:07:29 +0100

    initial commit - created infrastructure iac for the static webpage using storage account and blob

---
+++ iac/main.tf
@@ -0,0 +1,54 @@
+# Create a random name for the resource group using random_pet
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+# Create a resource group using the generated random name
+resource "azurerm_resource_group" "cloud-resume" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+#generate random string for storage account name
+resource "random_string" "storage_account_name" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+#create storage account for static website hosting
+resource "azurerm_storage_account" "storage_account" {
+  resource_group_name = azurerm_resource_group.cloud-resume.name
+  location            = azurerm_resource_group.cloud-resume.location
+
+  name = random_string.storage_account_name.result
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+}
+#enable static website hosting on the storage account
+resource "azurerm_storage_account_static_website" "static_website" {
+  storage_account_id = azurerm_storage_account.storage_account.id
+  error_404_document = "404.html"
+  index_document     = "index.html"
+}
+
+resource "azurerm_storage_blob" "website_blob" {
+  for_each = fileset("${path.module}/../frontend", "*")
+  name = each.value
+  storage_account_name = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type = "Block"
+  source = "${path.module}/../frontend/${each.value}"
+  depends_on = [azurerm_storage_account_static_website.static_website]
+}
+locals {
+  mime_types = {
+    ".html" = "text/html"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+  }
+}
